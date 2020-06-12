a1fe2b65883b174c2e6d56292a21ea77a0b5ec52
Glastis <glastis@glastis.com>
2020-06-12 18:45:09 +0200

    Added mouse down and up

--- autoclicker.py
+++ autoclicker.py
@@ -1,13 +1,20 @@
 import pyautogui
 from pynput.keyboard import *
+import ctypes
+from time import sleep
+
+ctypes.windll.kernel32.SetConsoleTitleW("Autoclicker")
 
 #  ======== settings ========
 delay = 1  # in seconds
 resume_key = Key.f1
 pause_key = Key.f2
+mouse_down = Key.f3
+mouse_up = Key.f4
 exit_key = Key.esc
 #  ==========================
 
+down = False
 pause = True
 running = True
 
@@ -20,21 +27,28 @@ def on_press(key):
     elif key == pause_key:
         pause = True
         print("[Paused]")
+    elif key == mouse_down:
+        pyautogui.mouseDown()
+        print("[Down]")
+    elif key == mouse_up:
+        pyautogui.mouseUp()
+        print("[Up]")
     elif key == exit_key:
         running = False
         print("[Exit]")
 
 
 def display_controls():
-    print("// AutoClicker by iSayChris")
+    print("// AutoClicker by iSayChris, forked by Glatis")
     print("// - Settings: ")
     print("\t delay = " + str(delay) + ' sec' + '\n')
     print("// - Controls:")
-    print("\t F1 = Resume")
-    print("\t F2 = Pause")
-    print("\t F3 = Exit")
+    print("\t F1 = Resume click spam")
+    print("\t F2 = Pause click spam")
+    print("\t F3 = Mouse down")
+    print("\t F4 = Mouse up")
     print("-----------------------------------------------------")
-    print('Press F1 to start ...')
+
 
 
 def main():
@@ -46,6 +60,8 @@ def main():
         if not pause:
             pyautogui.click(pyautogui.position())
             pyautogui.PAUSE = delay
+        else:
+            sleep(delay)
     lis.stop()
 
 
